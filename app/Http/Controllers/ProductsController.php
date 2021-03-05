6da9bc7356c03cb3abcc5cc478d36c31928adc48
Krishman Paudel <krishman98@users.noreply.github.com>
2021-03-06 02:24:37 +0545

    .env file, composer json/lock, packagist, vendor, routing, Http response, controllers, passing data to view and route parameters learnt

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    public function index(){
+        $title = "Welcome to my laravel 8 revision";
+        $description = "Created by Krishman";
+
+        $data = [
+            'one' => 'iphone',
+            'two' => 'samsung'
+        ];
+
+        //Compact Method
+        // return view('products.index', compact('title', 'description'));
+
+        // With Method
+        //return view('products.index')->with('data', $data); // ony useful to pass one data
+        // return view('products.index')->with('description', $description); cannot add
+
+        // Directly in the view
+        return view('products.index', [
+            'data' => $data
+        ]);
+    }
+
+    public function about(){
+        return 'This is about page';
+    }
+
+    public function show($name){
+        $data = [
+            'one' => 'iphone',
+            'two' => 'samsung'
+        ];
+        return view('products.index', [
+            'products' => $data[$name] ?? 'Product ' . $name . ' does not exist.'
+        ]);
+    }
+
+    // public function show($id){
+
+    //     return view('products.index', [
+    //         'id' => $id ?? 'Product id ' . $id . ' does not exist.'
+    //     ]);
+    // }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,57 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Laravel 8(new)
+Route::get('/products', [ProductsController::class, 'index']);
+Route::get('/products/about', [ProductsController::class, 'about']);
+
+// Pattern is integer
+// Route::get('/products/{id}', [ProductsController::class, 'show'])->where('id', '[0-9]+');
+
+//  Pattern is a string
+// Route::get('/products/{name}', [ProductsController::class, 'show'])->where('name', '[A-Z]+');
+
+Route::get('/products/{name}/{id}', [ProductsController::class, 'show'])->where([
+    'name' => '[a-zA-Z]+',
+    'id' => '[0-9]+'
+]);
+
+
+//Laravel 8(also new)
+// Route::get('/products', 'App\Http\Controllers\ProductsController@index');
+
+//Before Laravel 8
+//Route::get('/products', 'ProductsController@index'); doesnot work in laravel 8
+
+
+
+//Route that sends back a view
+// Route::get('/', function () {
+//     return view('home');
+// });
+
+//Route to users - String
+// Route::get('/users', function () {
+//     return 'Welcome to the users page';
+// });
+
+//Route to users- Array(JSON)
+// Route::get('/users', function () {
+//     return ['PHP', 'Laravel'];
+// });
+
+//Route to users - JSON Object
+// Route::get('/users', function () {
+//     return response()->json([
+//         'name' => 'Krishman',
+//         'course' => 'Laravel'
+//     ]);
+//     ;
+// });
+
+
+//Route to users- function
+
+// Route::get('/users', function() {
+//     return redirect('/');
+// });
